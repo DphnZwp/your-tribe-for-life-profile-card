4ec6f21e96e59b7956d5c857641301e91d42617f
DphnZwp <zwuupdaphne@gmail.com>
2022-09-08 15:22:01 +0200

    Flip animatie #3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
   // import * as prismicH from '@prismicio/helpers';
   export let data;
   console.log(data);
+  let flip = false;
 </script>
 
 <main>
     <!-- <pre>{JSON.stringify(data.document, null, 2)}</pre> -->
-  <section class="business-card">
+  <section class="business-card" class:flip="{flip}" on:click="{() => flip = !flip}">
     <article class="front">
       <h1>{data.document.title}</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta nisi maiores totam modi. Quidem repellendus ipsa praesentium architecto impedit! Quasi atque earum cupiditate excepturi dolorum mollitia recusandae dicta aspernatur magnam?</p>
@@ -20,4 +21,38 @@
 </main>
 
 <style>
+  .business-card.flip .front {
+    transform: rotateY(180deg)
+  }
+
+  .business-card.flip .back {
+    transform: rotateY(0deg)
+  }
+
+  .business-card {
+    perspective: 600px;
+  }
+
+  .front {
+    margin: 3em;
+    color: white;
+    background-color: purple;
+    position: absolute;
+    transform: rotateX(0deg) rotateY(0deg);
+    transform-style: preserve-3d;
+    transition: transform 4s cubic-bezier(0.2, 1, 0.3, 1.3);
+    backface-visibility: hidden;
+  }
+
+  .back {
+    margin: 3em;
+    color: white;
+    background-color: purple;
+    position: absolute;
+    transform: rotateY(-180deg);
+    transform-style: preserve-3d;
+    transform-style: preserve-3d;
+    transition: transform 4s cubic-bezier(0.2, 1, 0.3, 1.3);
+    backface-visibility: hidden;
+  }
 </style>
