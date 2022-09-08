42b828792f26ca69dd362c24752ffc6c8bddb9d2
DphnZwp <zwuupdaphne@gmail.com>
2022-09-08 13:24:53 +0200

    Foutmelding opgelost

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -5,10 +5,10 @@ import createClient from '$lib/prismicio';
 export async function load({ fetch, request }) {
     const UID = 'visitekaartje';
     const client = createClient({ fetch, request });
-    const document = await client.getByUID('visitekaartje', UID);
+    const document = await client.getByUID('page', UID);
 
     if (document) {
-        return { document };
+        return { document:document.data };
     }
 
     error(404, 'Not found');
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,22 @@
 <script>
   // import * as prismicH from '@prismicio/helpers';
   export let data;
+  console.log(data);
 </script>
 
 <main>
-
-  <pre>{JSON.stringify(data.document, null, 2)}</pre>
-
+    <!-- <pre>{JSON.stringify(data.document, null, 2)}</pre> -->
+  <section class="business-card">
+    <article class="front">
+      <h1>{data.document.title}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta nisi maiores totam modi. Quidem repellendus ipsa praesentium architecto impedit! Quasi atque earum cupiditate excepturi dolorum mollitia recusandae dicta aspernatur magnam?</p>
+    </article>
+    
+    <article class="back">
+      <h2>Kopje 2</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque unde voluptates ut nihil dolorem facilis cupiditate mollitia praesentium. Facere quo saepe minus, non vero rerum dignissimos dolorum fuga laudantium mollitia.</p>
+    </article>
+  </section>
 </main>
 
 <style>
